4aa82e56a4f864cb0ce60d0c32e4897913385fe9
MerinRoseGeorge <merinrgeorge@gmail.com>
2026-04-19 22:33:14 +0530

    Frontend core skeleton setup

---
+++ ml-algorithm-visualization-tool/app/about-team/page.js
@@ -0,0 +1,44 @@
+import Navbar from "../components/Navbar";
+
+export default function AboutTeamPage() {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="p-8">
+        <h1 className="text-3xl font-bold mb-4">
+          About Development Team
+        </h1>
+
+        <p>
+          Fr. C. Rodrigues Institute of Technology
+        </p>
+
+        <p>
+          Vashi, Navi Mumbai
+        </p>
+
+        <p>
+          Department of Computer Engineering
+        </p>
+
+        <p>
+          Subject: Full Stack Development Laboratory
+        </p>
+
+        <br />
+
+        <h2 className="text-xl font-bold">
+          Team Name: ML Innovators
+        </h2>
+
+        <p>Member 1 – Roll No.</p>
+        <p>Member 2 – Roll No.</p>
+        <p>Member 3 – Roll No.</p>
+        <p>Member 4 – Roll No.</p>
+        <p>Member 5 – Roll No.</p>
+        <p>Member 6 – Roll No.</p>
+      </div>
+    </div>
+  );
+}
